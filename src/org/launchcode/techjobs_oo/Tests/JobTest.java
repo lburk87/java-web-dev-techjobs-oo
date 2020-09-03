3bc68b6560fa31fc3e0f533223a82731909a7b1c
lburk87 <lburk87@gmail.com>
2020-09-03 08:19:45 -0500

    test package, JobTest Class, testSettingJobId()

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,25 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.assertFalse;
+
+public class JobTest {
+
+    Job test_job1;
+    Job test_job2;
+
+    @Before
+    public void jobObjects() {
+        test_job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        test_job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertFalse(test_job1.equals(test_job2));
+    }
+
+}
