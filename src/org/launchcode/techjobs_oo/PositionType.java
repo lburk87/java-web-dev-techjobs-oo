c026d6fae6be22c663fc9e738f45b97c64c3e495
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-09-09 18:48:58 -0500

    added AbstractEntity; CoreCompetency, Employer, Location, PositionType extends AbstractEntity

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -2,52 +2,45 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class PositionType {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
+public class PositionType extends AbstractEntity{
     public PositionType() {
-        id = nextId;
-        nextId++;
     }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
-    public String toString() {
-        return this.value;
-    }
+//    public String toString() {
+//        return this.value;
+//    }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
-    @Override
-    public boolean equals(Object o) {
-        PositionType that = (PositionType) o;
-        return getId() == that.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getValue());
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        PositionType that = (PositionType) o;
+//        return getId() == that.getId();
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId(), getValue());
+//    }
 
     // Getters and Setters:
 
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 
 
 }
